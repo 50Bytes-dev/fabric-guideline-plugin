a1869de3564478cdd12c8d86b60dd816b20268ea
PranovAdilet <pranov.adiletqwe@mail.ru>
2024-09-19 17:42:59 +0600

    update

--- src/module/index.ts
+++ src/module/index.ts
@@ -21,6 +21,10 @@ function generateLightColorRgb() {
   return "rgb(" + red + ", " + green + ", " + blue + ")";
 }
 
+interface CustomFabricObject extends fabric.Object {
+  myType?: string;
+}
+
 function setupObjects() {
   global.outer = new fabric.Rect({
     width: fabricCanvas.getWidth(),
@@ -31,10 +35,16 @@ function setupObjects() {
     evented: false,
     fill: "#ececec",
     selectable: false,
-  });
+  }) as CustomFabricObject;
 
   fabricCanvas.add(global.outer);
-  global.outer.center();
+  fabricCanvas.renderAll();
+
+  if (typeof global.outer.center === "function") {
+    global.outer.center();
+  } else {
+    console.error("Method center is not available on fabric.Rect");
+  }
 
   const genRect = (
     {
@@ -63,12 +73,47 @@ function setupObjects() {
       fabricCanvas.add(genRect());
     }
   }
-  let allBoxes = new fabric.ActiveSelection(
-    fabricCanvas.getObjects().filter((obj) => obj.myType == "box"),
-    { canvas: fabricCanvas }
-  );
-  allBoxes.center();
-  allBoxes.destroy();
+
+  const boxObjects = fabricCanvas.getObjects().filter((obj: CustomFabricObject) => obj.myType === "box");
+
+  if (boxObjects.length > 0) {
+    // Центрируем все объекты вручную
+    const boundingRect = getBoundingRect(boxObjects);
+    const canvasWidth = fabricCanvas.getWidth();
+    const canvasHeight = fabricCanvas.getHeight();
+    const centerX = (canvasWidth - boundingRect.width) / 2;
+    const centerY = (canvasHeight - boundingRect.height) / 2;
+
+    boxObjects.forEach((obj) => {
+      obj.set({
+        left: obj.left + centerX - boundingRect.left,
+        top: obj.top + centerY - boundingRect.top,
+      });
+    });
+
+    fabricCanvas.renderAll();
+  }
+}
+
+function getBoundingRect(objects: fabric.Object[]) {
+  if (objects.length === 0) return { left: 0, top: 0, width: 0, height: 0 };
+
+  const lefts = objects.map((obj) => obj.left || 0);
+  const tops = objects.map((obj) => obj.top || 0);
+  const rights = objects.map((obj) => (obj.left || 0) + (obj.width || 0));
+  const bottoms = objects.map((obj) => (obj.top || 0) + (obj.height || 0));
+
+  const left = Math.min(...lefts);
+  const top = Math.min(...tops);
+  const right = Math.max(...rights);
+  const bottom = Math.max(...bottoms);
+
+  return {
+    left,
+    top,
+    width: right - left,
+    height: bottom - top,
+  };
 }
 
 let resetButton = document.getElementById("reset") as any;
@@ -97,39 +142,22 @@ function reset() {
 fabricCanvas.on("mouse:wheel", (opt) => {
   let delta = 0;
 
-  // -------------------------------
-  // WHEEL RESOLUTION
-  let wheelDelta = opt.e.wheelDelta;
   let deltaY = opt.e.deltaY;
 
-  // CHROME WIN/MAC | SAFARI 7 MAC | OPERA WIN/MAC | EDGE
-  if (wheelDelta) {
-    delta = -wheelDelta / 120;
-  }
-  // FIREFOX WIN / MAC | IE
   if (deltaY) {
-    deltaY > 0 ? (delta = 1) : (delta = -1);
+    delta = deltaY > 0 ? 1 : -1;
   }
-  // -------------------------------
 
   let pointer = fabricCanvas.getPointer(opt.e);
   let zoom = fabricCanvas.getZoom();
   zoom = zoom - delta / 10;
 
-  // limit zoom in
+  // Ограничьте масштабирование
   if (zoom > 4) zoom = 4;
+  if (zoom < 0.2) zoom = 0.2;
 
-  // limit zoom out
-  if (zoom < 0.2) {
-    zoom = 0.2;
-  }
-
-  //canvas.zoomToPoint({
-  //  x: opt.e.offsetX,
-  //  y: opt.e.offsetY
-  //}, zoom)
-
-  fabricCanvas.zoomToPoint(new fabric.Point(fabricCanvas.width / 2, fabricCanvas.height / 2), zoom);
+  // Примените масштабирование
+  fabricCanvas.zoomToPoint(new fabric.Point(pointer.x, pointer.y), zoom);
 
   opt.e.preventDefault();
   opt.e.stopPropagation();
@@ -137,16 +165,6 @@ fabricCanvas.on("mouse:wheel", (opt) => {
   fabricCanvas.renderAll();
   fabricCanvas.calcOffset();
 });
-
-// initCenteringGuidelines(fabricCanvas);
-// initAligningGuidelines(canvas);
-
-// ==========================================
-// CANVAS CENTER SNAPPING & ALIGNMENT GUIDELINES
-// ==========================================
-
-// ORIGINAL:
-// https://github.com/fabricjs/fabric.js/blob/master/lib/centering_guidelines.js
 
 /**
  * Augments canvas by assigning to `onObjectMove` and `onAfterRender`.
@@ -189,29 +207,32 @@ function initCenteringGuidelines(canvas: fabric.Canvas) {
   }
 
   function showCenterLine(x1: number, y1: number, x2: number, y2: number) {
-    const originXY = fabric.util.transformPoint(new fabric.Point(x1, y1), canvas.viewportTransform as number[]),
-      dimensions = fabric.util.transformPoint(new fabric.Point(x2, y2), canvas.viewportTransform as number[]);
+    const transform = canvas.viewportTransform as fabric.TMat2D;
+
+    const originXY = fabric.util.transformPoint(new fabric.Point(x1, y1), transform);
+    const dimensions = fabric.util.transformPoint(new fabric.Point(x2, y2), transform);
+
     ctx.save();
     ctx.strokeStyle = centerLineColor;
     ctx.lineWidth = centerLineWidth;
     ctx.beginPath();
 
     ctx.moveTo(originXY.x, originXY.y);
-
     ctx.lineTo(dimensions.x, dimensions.y);
     ctx.stroke();
     ctx.restore();
   }
 
-  let afterRenderActions = [],
-    isInVerticalCenter,
-    isInHorizontalCenter;
+  let afterRenderActions: any[] = [];
+  let isInVerticalCenter: boolean | null = null;
+  let isInHorizontalCenter: boolean | null = null;
 
   canvas.on("mouse:down", () => {
-    isInVerticalCenter = isInHorizontalCenter = null;
+    isInVerticalCenter = null;
+    isInHorizontalCenter = null;
     global.centerLine_horizontal = "";
     global.centerLine_vertical = "";
-    viewportTransform = canvas.viewportTransform;
+    viewportTransform = canvas.viewportTransform as fabric.TMat2D;
   });
 
   canvas.on("object:moving", function (e) {
--- src/module/util.ts
+++ src/module/util.ts
@@ -1,12 +1,12 @@
-export const Keys: <T = Record<string, any>>(obj: T) => (keyof T)[] = obj => {
-  return Object.keys(obj) as any;
+export const Keys: <T = Record<string, any>>(obj: T) => (keyof T)[] = (obj) => {
+  return Object.keys(obj as any) as any;
 };
 
 export function omit<T extends Record<string, unknown>, K extends keyof T>(obj: T, fields: K[]): Omit<T, K> {
   const clone = { ...obj };
 
   if (Array.isArray(fields)) {
-    fields.forEach(key => {
+    fields.forEach((key) => {
       delete clone[key];
     });
   }
