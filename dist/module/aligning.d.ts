9a28b48e1898635963db4b7051691de1bc627cf2
PranovAdilet <pranov.adiletqwe@mail.ru>
2024-09-19 19:24:53 +0600

    update

---
+++ dist/module/aligning.d.ts
@@ -0,0 +1,62 @@
+import * as fabric from "fabric";
+type VerticalLineCoords = {
+    x: number;
+    y1: number;
+    y2: number;
+};
+type HorizontalLineCoords = {
+    y: number;
+    x1: number;
+    x2: number;
+};
+type IgnoreObjTypes = {
+    key: string;
+    value: any;
+}[];
+export declare class AlignGuidelines {
+    aligningLineMargin: number;
+    aligningLineWidth: number;
+    aligningLineColor: string;
+    ignoreObjTypes: IgnoreObjTypes;
+    pickObjTypes: IgnoreObjTypes;
+    canvas: fabric.Canvas;
+    ctx: CanvasRenderingContext2D;
+    viewportTransform: any;
+    verticalLines: VerticalLineCoords[];
+    horizontalLines: HorizontalLineCoords[];
+    activeObj: fabric.Object;
+    constructor({ canvas, aligningOptions, ignoreObjTypes, pickObjTypes, }: {
+        canvas: fabric.Canvas;
+        ignoreObjTypes?: IgnoreObjTypes;
+        pickObjTypes?: IgnoreObjTypes;
+        aligningOptions?: {
+            lineMargin?: number;
+            lineWidth?: number;
+            lineColor?: string;
+        };
+    });
+    private drawSign;
+    private drawLine;
+    private drawVerticalLine;
+    private drawHorizontalLine;
+    private isInRange;
+    private watchMouseDown;
+    private watchMouseUp;
+    private watchMouseWheel;
+    private clearLinesMeta;
+    private watchObjectMoving;
+    private getObjDraggingObjCoords;
+    private omitCoords;
+    private getObjMaxWidthHeightByCoords;
+    /**
+     * fabric.Object.getCenterPoint will return the center point of the object calc by mouse moving & dragging distance.
+     * calcCenterPointByACoords will return real center point of the object position.
+     */
+    private calcCenterPointByACoords;
+    private traversAllObjects;
+    private snap;
+    clearGuideline(): void;
+    watchRender(): void;
+    init(): void;
+}
+export {};
